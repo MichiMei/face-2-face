251e2679c195a27a9b730ccf668e7e9b100c8295
Chris Gabriel Pawelec <pawelecc@student.hu-berlin.de>
2021-04-23 14:49:34 +0200

    add maven to project

---
+++ p2projekt21/src/main/java/huberlin/p2projekt21/App.java
@@ -0,0 +1,13 @@
+package huberlin.p2projekt21;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
